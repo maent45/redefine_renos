b7a49c7b9bc346b93485c3a73c93875541172e77
maent45 <maent45@gmail.com>
2015-10-02 17:35:01 +1300

    First skeleton push -moved static site to silverstripe

---
+++ Redefine_Renovations/README.md
@@ -0,0 +1,16 @@
+# redefine_renos
+A static web template which will later be implemented into Silverstripe for a CMS.
+
+External Plugins used:
+
+  The following are lists of external jQuery plugins that have been used in this template:
+  
+    - mixItUp:
+    
+      - To allow filtering and sorting of images based on their categories.
+      
+    - lightBox:
+    
+      - To allow for swiping of images on tablet and smartphone screens.
+      
+      - Responsive image gallery plugin, compatible on different screen resolutions.
---
+++ Redefine_Renovations/js/custom.js
@@ -0,0 +1,71 @@
+/**
+ * Created by Maen Terawasi on 23/09/2015.
+ */
+
+$(function(){
+    //Instantiate MixItUp:
+    $('#Container').mixItUp();
+
+//Instantiate sliphover
+    //$(".featuredWorksWrapper").sliphover();
+
+    /*$('#d1').contenthover({
+        effect:'slide',
+        slide_speed:300,
+        overlay_background:'#000',
+        overlay_opacity:0.8
+    });*/
+
+    //hide hover effect on dom load
+    $('.imageHoverEffect').hide();
+    //trigger image effect on hover
+    $('.featuredWorksGallery .mix').hover(function(element) {
+        $('.imageHoverEffect',$(this)).stop().slideToggle();
+    });
+
+    //Full gallery plugin options with default values.
+    var settings = {
+        // shows fullscreen overlay
+        overlay:    true,
+        // shows loading spinner
+        spinner:    true,
+        // shows navigation arrows
+        nav:      true,
+        // text for navigation arrows
+        navText:    ['&larr;','&rarr;'],
+        // shows image captions
+        captions:   true,
+        // captions attribute (title or data-title)
+        captionsData: 'title',
+        // shows close button
+        close:      true,
+        // text for close button
+        closeText:    'X',
+        // show counter
+        showCounter:	true,
+        // file extensions
+        fileExt:    'png|jpg|jpeg|gif',
+        // animation speed in ms
+        animationSpeed: 250,
+        // image preloading
+        preloading:   true,
+        // keyboard navigation
+        enableKeyboard: true,
+        // endless looping
+        loop:     true,
+        // closes the lightbox when clicking outside
+        docClose:     true,
+        // how much pixel you have to swipe
+        swipeTolerance: 50,
+        // lightbox wrapper Class
+        className:    'simple-lightbox',
+        // width / height ratios
+        widthRatio:   0.8,
+        heightRatio:  0.9
+    };
+    //Call gallery the plugin with default options to enable the lightbox.
+    var gallery = $('.gallery a').simpleLightbox(settings);
+});
+
+
+
