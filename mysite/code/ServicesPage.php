7059cbb3246a4807de5e5641019022646c6cfb83
maent45 <maent45@gmail.com>
2015-10-04 13:13:10 +1300

    added ServicesPage db fields created db image fields, set image upload folder, set image type validation

--- mysite/code/ServicesPage.php
+++ mysite/code/ServicesPage.php
@@ -23,22 +23,64 @@ class ServicesPage extends Page {
         'ServiceDesc3' => 'Text'
     );
 
+    //create database image fields
+    private static $has_one = array (
+        'ServiceImage1' => 'Image',
+        'ServiceImage2' => 'Image',
+        'ServiceImage3' => 'Image'
+    );
+
     //update CMS interface
     public function getCMSFields() {
         //declare $fields var
         $fields = parent::getCMSFields();
 
+        //create read-only fields
+        $fields->addFieldToTab('Root.Metadata', new ReadonlyField('URLSegment', 'URL'));
+        $fields->removeFieldFromTab('Root.Content.Metadata', 'MenuTitle');
+        $fields->addFieldToTab("Root.Content.Metadata", new ReadonlyField('MenuTitle','URL'));
+
+        /*--- remove the default HTML editor section from CMS ---*/
+        $fields->removeFieldFromTab("Root.Main","Content");
+        //temporarily remove MetaData field
+        $fields->removeByName('Metadata');
+
         //create page header input fields
         $fields->addFieldToTab('Root.Main', TextField::create('PageHeader', 'Page Heading'));
-        $fields->addFieldToTab('Root.Main', TextField::create('PageSubHeader', 'Page Sub Heading'));
+        $fields->addFieldToTab('Root.Main', TextareaField::create('PageSubHeader', 'Page Sub Heading'));
 
-        //create services input fields
+        //add image upload fields
+        $fields->addFieldToTab('Root.Services', $ServiceImage1 = new UploadField('ServiceImage1','Service 1 Image'));
         $fields->addFieldToTab('Root.Services', TextField::create('ServiceHead1', 'Service 1 Title'));
-        $fields->addFieldToTab('Root.Services', TextField::create('ServiceDesc1', 'Service 1 Description'));
+        $fields->addFieldToTab('Root.Services', TextareaField::create('ServiceDesc1', 'Service 1 Description'));
+
+        //add image upload fields
+        $fields->addFieldToTab('Root.Services', $ServiceImage2 = new UploadField('ServiceImage2','Service 2 Image'));
         $fields->addFieldToTab('Root.Services', TextField::create('ServiceHead2', 'Service 2 Title'));
-        $fields->addFieldToTab('Root.Services', TextField::create('ServiceDesc2', 'Service 2 Description'));
+        $fields->addFieldToTab('Root.Services', TextareaField::create('ServiceDesc2', 'Service 2 Description'));
+
+        //add image upload fields
+        $fields->addFieldToTab('Root.Services', $ServiceImage3 = new UploadField('ServiceImage3','Service 3 Image'));
         $fields->addFieldToTab('Root.Services', TextField::create('ServiceHead3', 'Service 3 Title'));
-        $fields->addFieldToTab('Root.Services', TextField::create('ServiceDesc3', 'Service 3 Description'));
+        $fields->addFieldToTab('Root.Services', TextareaField::create('ServiceDesc3', 'Service 3 Description'));
+
+        //validate image types
+        $ServiceImage1->getValidator()->setAllowedExtensions(array(
+            'png','gif','jpg','jpeg'
+        ));
+        $ServiceImage2->getValidator()->setAllowedExtensions(array(
+            'png','gif','jpg','jpeg'
+        ));
+        $ServiceImage3->getValidator()->setAllowedExtensions(array(
+            'png','gif','jpg','jpeg'
+        ));
+        //set image upload folder
+        $ServiceImage1->setFolderName('Service Page Images');
+        $ServiceImage2->setFolderName('Service Page Images');
+        $ServiceImage3->setFolderName('Service Page Images');
+
+        //return the fields
+        return $fields;
     }
 }
 
