f7d99a1c37e9f00f6af60b1719dd9919dde8c61e
Luke Hardiman <luke@hardiman.co.nz>
2015-10-11 00:01:24 +1300

    Structure of buildersCrack module -starting structure of builderscrack module

---
+++ module_buildersCrack/_config.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Basics of config file
+define('MODULE_BUILDERSCRACK_DIR', basename(dirname(__FILE__)));
+
+DataObject::add_extension('SiteTree', 'BuildersCrack');
+
+ShortcodeParser::get()->register('builders_crack_item', array('BuildersCrack', 'BuildersCrackShortcodeHandler'));
+*/
