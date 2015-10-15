720259718deb9a96276531e020a56427577c3c5c
Luke Hardiman <luke@hardiman.co.nz>
2015-10-15 22:00:08 +1300

    Updating buildersCrack module -added sandbox starting module as extension

--- module_buildersCrack/code/BuildersCrack.php
+++ module_buildersCrack/code/BuildersCrack.php
@@ -3,7 +3,7 @@
 class BuildersCrack extends DataExtension {
 	
 	protected static $url = "https://builderscrack.co.nz/";	//url of the review to be scrapped
-
+	protected static $sandBox = false;
 
 	/**
 	 * @param $url URL of builders crack review to be scrapped
@@ -12,6 +12,17 @@ class BuildersCrack extends DataExtension {
 		self::$url = $url;
 	}
 
+	/**
+	 * @param $sandbox bool sets sandbox mode
+	 */
+	public static function set_sandbox($sandbox)
+	{
+		self::$sandBox = $sandbox;
+	}
 
+	public static function BuildersCrackItemHandler($attributes, $content = null, $parser = null)
+	{
+
+	}
 	
 }
